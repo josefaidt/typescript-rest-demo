815b0abab4eb9be4d544071b709f8623df5270a4
josefaidt <josef.aidt@gmail.com>
2020-07-20 09:27:39 -0500

    feat: working service

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ import FSRouter from 'express-fs-router'
 const PORT: number = parseFloat(process.env.PORT) || 3000
 const app = express()
 
-// app.get('/', (req: Request, res: Response) => res.json({ message: 'hello' }))
-app.use('/', new FSRouter(path.join(__dirname, 'api')))
+app.get('/test', (req: Request, res: Response) => res.json({ message: 'hello' }))
+app.use('/api', new FSRouter(path.join(__dirname, 'api')))
 
 app.listen(PORT, () => console.info(`Listening at http://localhost:${PORT}`))
